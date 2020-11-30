c3fa1fa3e2090af9de5a1b2e14629869017b4990
lzkrishmom <lzkrishmom@gmail.com>
2020-11-30 08:11:53 -0500

    finalized
    finalized

---
+++ calculator-app/src/symbolKeys.js
@@ -0,0 +1,117 @@
+export const symbolKeys = [
+    {
+        keycode: 48,
+        keyTrigger: "0"
+    },
+    {
+        keycode: 96,
+        keyTrigger: "0"
+    },
+    {
+        keycode: 49,
+        keyTrigger: "1"
+    },
+    {
+        keycode: 97,
+        keyTrigger: "1"
+    },
+    {
+        keycode: 50,
+        keyTrigger: "2"
+    },
+    {
+        keycode: 98,
+        keyTrigger: "2"
+    },
+    {
+        keycode: 51,
+        keyTrigger: "3"
+    },
+    {
+        keycode: 99,
+        keyTrigger: "3"
+    },
+    {
+        keycode: 52,
+        keyTrigger: "4"
+    },
+    {
+        keycode: 100,
+        keyTrigger: "4"
+    },
+    {
+        keycode: 53,
+        keyTrigger: "5"
+    },
+    {
+        keycode: 101,
+        keyTrigger: "5"
+    },
+    {
+        keycode: 54,
+        keyTrigger: "6"
+    },
+    {
+        keycode: 102,
+        keyTrigger: "6"
+    },
+    {
+        keycode: 55,
+        keyTrigger: "7"
+    },
+    {
+        keycode: 103,
+        keyTrigger: "7"
+    },
+    {
+        keycode: 56,
+        keyTrigger: "8"
+    },
+    {
+        keycode: 104,
+        keyTrigger: "8"
+    },
+    {
+        keycode: 57,
+        keyTrigger: "9"
+    },
+    {
+        keycode: 105,
+        keyTrigger: "9"
+    },
+    {
+        keycode: 191,
+        keyTrigger: "/"
+    },
+    {
+        keycode: 111,
+        keyTrigger: "/"
+    },
+    {
+        keycode: 106,
+        keyTrigger: "*"
+    },
+    {
+        keycode: 107,
+        keyTrigger: "+"
+    },
+    {
+        keycode: 109,
+        keyTrigger: "-"
+    },
+    {
+        keycode: 13,
+        keyTrigger: "="
+    },
+    {
+        keycode: 110,
+        keyTrigger: "."
+    },
+    {
+        keycode: 8,
+        keyTrigger: "C"
+    }
+    
+];
+
+export default symbolKeys
